0798b40bcd653ba80d8739868d98fdb7c2975fda
kentaro hirahara <kentaro199910@gmail.com>
2025-06-22 02:43:56 +0900

    add CameraSample

---
+++ MySandboxApp/20250622_CameraSample/CameraSampleMainView.swift
@@ -0,0 +1,18 @@
+//
+//  CameraSampleMainView.swift
+//  MySandboxApp
+//
+//  Created by 平原健太郎 on 2025/06/22.
+//
+
+import SwiftUI
+
+struct CameraSampleMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CameraSampleMainView()
+}
---
+++ MySandboxApp/20250622_CameraSample/ObjectDetectionModel.swift
@@ -0,0 +1,8 @@
+//
+//  ObjectDetectionModel.swift
+//  MySandboxApp
+//
+//  Created by 平原健太郎 on 2025/06/22.
+//
+
+import Foundation
