5db7df7045ff2683cc383736852f8907d904f36b
kentaro hirahara <kentaro199910@gmail.com>
2025-06-21 13:57:12 +0900

    Initial Commit

---
+++ MySandboxApp/MySandboxAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySandboxAppApp.swift
+//  MySandboxApp
+//
+//  Created by 平原健太郎 on 2025/06/21.
+//
+
+import SwiftUI
+
+@main
+struct MySandboxAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
